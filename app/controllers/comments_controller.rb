b8e302493a7a3440d8ab4d00d012e3c74cb3bc83
Zulfa <zulfa@rockpapersystems.com>
2017-01-19 18:24:23 +0700

    added css and WYSIWYG

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,8 @@ class CommentsController < ApplicationController
 
   def destroy
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
+    #@comment = @article.comments.find(params[:id])
+    #@comment.destroy
     redirect_to article_path(@article)
   end
 
---
+++ vendor/assets/javascripts/tinymce-rails-master/lib/tinymce/rails/version.rb
@@ -0,0 +1,6 @@
+module TinyMCE
+  module Rails
+    VERSION = "4.5.2"
+    TINYMCE_VERSION = "4.5.2"
+  end
+end
